928af30f24305de1cdf049e0a15faf7e25f66d29
Tinco <tincohxr@qq.com>
2020-05-28 22:01:44 +0800

    STD-V1.0

--- code/test/test.py
+++ code/test/test.py
@@ -8,20 +8,10 @@ def test_obstacle_backward():
 	test = subprocess.Popen("rosrun my_vel_package move_backward");
 	test2 = subprocess.Popen("rosrun obstacle my_obstacle_detect > test_result_backward.txt");
 
-def test_obstacle_():
+def test_obstacle_right():
 	
 
 
-def test_obstacle_():
+def test_obstacle_left():
 
 
-
-def test_obstacle_():
-
-
-
-def test_obstacle_():
-
-
-
-def test_obstacle_():
